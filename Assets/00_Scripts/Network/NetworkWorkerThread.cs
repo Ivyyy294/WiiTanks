cd38fc458b75f8f92d28a48ce71a8785c854ed8b
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-11-07 13:46:46 +0100

    :sparkles: NEW NetworkWorkerThread

---
+++ Assets/00_Scripts/Network/NetworkWorkerThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+public abstract class NetworkWorkerThread
+{
+	private Thread thread = null;
+	protected bool shutdown = false;
+
+	public void Start()
+	{
+		thread = new Thread (ReceiveData);
+		thread.IsBackground = true;
+		thread.Start();
+	}
+
+	public void Shutdown()
+	{
+		shutdown = true;
+		thread.Join();
+	}
+
+	protected abstract void ReceiveData();
+	protected abstract bool SendData();
+}
