7cc9b2da77a583c45eb55aee451affb1fe7e9c2e
um6ra <87470563+um6ra@users.noreply.github.com>
2023-11-21 16:54:31 +0100

    changed reference from projectile Script

--- Assets/00_Scripts/Ship/Projectile.cs
+++ Assets/00_Scripts/Ship/Projectile.cs
@@ -6,15 +6,21 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float velocity = 5f;
     [SerializeField] private float damage;
+    private Rigidbody _rigidbody;   
+
     public float Damage
     {
         get { return damage; }
     }
 
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();    
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        gameObject.GetComponent<Rigidbody>().MovePosition(gameObject.transform.position + gameObject.transform.forward * velocity * Time.fixedDeltaTime);
+        _rigidbody.MovePosition(gameObject.transform.position + gameObject.transform.forward * velocity * Time.fixedDeltaTime);
     }
 }
